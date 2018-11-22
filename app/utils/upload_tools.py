951980bf952d25741b40b0e7a92521b6a43a8fb3
CCG CyberDefense Group <ccg.cyber@gmail.com>
2018-11-21 20:38:32 -0500

    altered comments

--- app/utils/upload_tools.py
+++ app/utils/upload_tools.py
@@ -1,17 +1,17 @@
 #coding:UTF-8
-__author__ = 'dj'
+__author__ = 'CCG'
 
 import random
 
-#上传后缀名校验
+# Upload suffix name check
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = set(['pcap', 'cap'])
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-#获取文件后缀
+# Get file suffix
 def get_filetype(filename):
     return '.' + filename.rsplit('.', 1)[1]
 
-#生成随机的字符串文件名
+# Generate a random string file name
 def random_name():
     return ''.join(random.sample('1234567890qazxswedcvfrtgbnhyujmkiolp', 10))
