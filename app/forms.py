173b345bf09a486d6bcab1ae4d4293af283f3fa5
CCG CyberDefense Group <ccg.cyber@gmail.com>
2019-04-30 11:18:42 -0400

    move Form --> FlaskForm

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
 #coding:UTF-8
 __author__ = 'dj'
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from wtforms.validators import DataRequired, AnyOf
 
 # Upload form
-class Upload(Form):
+class Upload(FlaskForm):
     pcap = FileField('pcap', validators=[DataRequired()])
 
 # Protocol filter form
-class ProtoFilter(Form):
+class ProtoFilter(FlaskForm):
     value = FileField('value')
     filter_type = FileField('filter_type', validators=[DataRequired(), AnyOf([u'all', u'proto', u'ipsrc', u'ipdst'])])
